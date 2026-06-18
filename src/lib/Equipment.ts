21b08279f8b3bde097c7adacdb590c20fee76b06
koropokkuru <202783065+koropokguru@users.noreply.github.com>
2026-06-19 01:43:50 +0200

    added crimson kisten

--- src/lib/Equipment.ts
+++ src/lib/Equipment.ts
@@ -523,6 +523,7 @@ export const WEAPON_SPEC_COSTS: { [canonicalName: string]: number } = {
   'Abyssal whip': 50,
   'Barrelchest anchor': 50,
   'Eye of ayak': 50,
+  'Crimson Kisten': 50,
 
   'Magic shortbow': 55,
   'Dark bow': 55,
